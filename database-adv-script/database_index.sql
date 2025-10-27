96311356c1b746f97cf9fe8a0d2c5cfe7a0de2b0
Asaad <asaad1793.z@gmail.com>
2025-10-27 21:09:31 +0300

    subquries

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,11 @@
 -- Create indexes to improve performance
 
+-- 1️⃣ قبل إنشاء الفهارس: تحليل الأداء الحالي
+EXPLAIN ANALYZE 
+SELECT * 
+FROM booking 
+WHERE user_id = 'some-user-id';
+
 -- 🧍 User table indexes
 CREATE INDEX idx_user_email ON user(email);
 CREATE INDEX idx_user_role ON user(role);
@@ -17,3 +23,15 @@ CREATE INDEX idx_property_price ON property(price_per_night);
 SHOW INDEX FROM user;
 SHOW INDEX FROM booking;
 SHOW INDEX FROM property;
+
+
+-- 3️⃣ بعد إنشاء الفهارس: إعادة تحليل الأداء
+EXPLAIN ANALYZE 
+SELECT * 
+FROM booking 
+WHERE user_id = 'some-user-id';
+
+-- ملاحظات:
+-- ⬆️ EXPLAIN ANALYZE بيعرض خطة التنفيذ (Execution Plan)
+-- وبيوضح وقت التنفيذ قبل وبعد الفهارس
+-- بحيث تقدر تشوف فعلاً هل الأداء اتحسن ولا لأ
